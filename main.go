8450d014a8730eb69c14f1d24a787b0adaf8883f
zachthomas823 <zachthomas823@gmail.com>
2020-01-08 21:33:06 -0600

    cleaned up player display functionality
    The printing of the player statistics has been changed for readability and moved into a function called printPlayer()

--- main.go
+++ main.go
@@ -7,9 +7,19 @@ import (
 	"strings"
 )
 
+func printPlayer(header []string, stats []string) {
+	for i := 0; i < len(header); i++ {
+		if header[i] == "Player" {
+			stats[i] = strings.Split(stats[i], "\\")[0]
+		}
+		fmt.Println(header[i] + " - " + stats[i])
+	}
+}
+
 func main() {
 	nbaStats, _ := os.Open("./nba_data.csv")
 	csvReader := csv.NewReader(nbaStats)
+	header, _ := csvReader.Read()
 	playerName := os.Args[1]
 	contin := true
 	found := false
@@ -18,7 +28,7 @@ func main() {
 		if error != nil {
 			contin = false
 		} else if strings.Split(line[1], "\\")[0] == playerName {
-			fmt.Println(line)
+			printPlayer(header, line)
 			found = true
 			contin = false
 		}
