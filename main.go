09f1aeb277c13c166125f36d734c6954cfa7700f
zachthomas823 <zachthomas823@gmail.com>
2020-01-10 10:03:03 -0600

    Added functionality to look up specific stat
    The user can use the tag "stat" and give the name of a stat to look up just that stat. Next will sort and give top 25 players.

--- main.go
+++ main.go
@@ -9,14 +9,27 @@ import (
 )
 
 var player bool
+var stat bool
 
 func init() {
+}
+
+func main() {
+	flag.BoolVar(&player, "player", false, "get player statistics for name passed")
+	flag.BoolVar(&stat, "stat", false, "get the leaders for an individual statistic")
+	flag.Parse()
+	if player {
+		playerStats()
+	} else if stat {
+		statLeader()
+	}
 }
 
 func playerStats() {
 	nbaStats, _ := os.Open("./nba_data.csv")
 	csvReader := csv.NewReader(nbaStats)
 	header, _ := csvReader.Read()
+	fmt.Println(header)
 	playerName := os.Args[2]
 	contin := true
 	found := false
@@ -44,10 +57,42 @@ func printPlayer(header []string, stats []string) {
 	}
 }
 
-func main() {
-	flag.BoolVar(&player, "player", false, "get player statistics for name passed")
-	flag.Parse()
-	if player {
-		playerStats()
+func statLeader() {
+	nbaStats, _ := os.Open("./nba_data.csv")
+	csvReader := csv.NewReader(nbaStats)
+	header, _ := csvReader.Read()
+	fmt.Println(header)
+	stat := os.Args[2]
+	var statIdx int
+	var found bool
+	for i := 0; i < len(header); i++ {
+		if header[i] == stat {
+			statIdx = i
+			found = true
+			break
+		}
+	}
+	if !found {
+		fmt.Println("Couldn't find that stat")
+	} else {
+		var allStat []string
+		var names []string
+		contin := true
+		for contin {
+			line, err := csvReader.Read()
+			if err != nil {
+				contin = false
+			} else {
+				allStat = append(allStat, line[statIdx])
+				names = append(names, strings.Split(line[1], "\\")[0])
+			}
+		}
+		printStatLeader(names, allStat)
+	}
+}
+
+func printStatLeader(names []string, stats []string) {
+	for i := 0; i < len(names); i++ {
+		fmt.Println(i+1, ":", names[i], "\t", stats[i])
 	}
 }
