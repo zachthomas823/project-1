dddf6d3d81c61284c756ae3358e568239c093f1c
zachthomas823 <zachthomas823@gmail.com>
2020-01-16 21:13:37 -0600

    web/statd.go now prints something to http server

--- web/statd.go
+++ web/statd.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/project-0/config"
+	"github.com/project-0/stats"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, World!")
+		if config.Player {
+			fmt.Fprint(w, stats.PlayerStats())
+		} else if config.Stat {
+			fmt.Fprint(w, stats.StatLeader())
+		} else {
+			fmt.Fprint(w, "Please use tag -player \"Player Name\" \nor tag -stat \"Stat abrev\"")
+		}
 	})
 	http.ListenAndServe(":8080", nil)
 }
